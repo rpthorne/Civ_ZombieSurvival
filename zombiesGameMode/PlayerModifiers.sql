0ca2287560c328d47263d7178ee3034c3afa3683
Ryan Thorne <rthorne3553@gmail.com>
2020-04-18 20:11:18 -0400

    initial commit

---
+++ zombiesGameMode/PlayerModifiers.sql
@@ -0,0 +1,15 @@
+-- PlayerModifiers
+-- Author: newto
+-- DateCreated: 4/9/2020 10:07:47 PM
+--------------------------------------------------------------
+
+
+INSERT INTO GameModifiers (ModifierId)
+VALUES ('GAME_REDUCED_LOYALTY_RADIUS'),
+('GAME_REDUCED_LOYALTY_RADIUS');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permenant, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('','','','','',''),
+('','','','','',''),
+('GAME_REDUCED_LOYALTY_RADIUS','','','','','');
+
